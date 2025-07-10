93b88048cc0a68ceb2d5b93a3e402e84da088896
tommy <mzt.live@live.com>
2025-07-11 00:25:02 +0800

    测试loading

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
 // src/main.rs
 use anyhow::Result;
+use embedded_graphics::pixelcolor::Rgb565;
 use esp_idf_hal::{delay::FreeRtos, peripherals::Peripherals};
 
 mod graphics;
 mod lcd;
 mod lcd_cmds;
 use lcd::LcdController;
+use tinybmp::Bmp;
 
 use crate::graphics::{colors::WHITE, primitives::GraphicsPrimitives};
+
+// 定义donghua动画图片数据数组
+const DONGHUA_IMAGES: [&[u8]; 12] = [
+    include_bytes!("../assets/donghua/1.bmp"),
+    include_bytes!("../assets/donghua/2.bmp"),
+    include_bytes!("../assets/donghua/3.bmp"),
+    include_bytes!("../assets/donghua/4.bmp"),
+    include_bytes!("../assets/donghua/5.bmp"),
+    include_bytes!("../assets/donghua/6.bmp"),
+    include_bytes!("../assets/donghua/7.bmp"),
+    include_bytes!("../assets/donghua/8.bmp"),
+    include_bytes!("../assets/donghua/9.bmp"),
+    include_bytes!("../assets/donghua/10.bmp"),
+    include_bytes!("../assets/donghua/11.bmp"),
+    include_bytes!("../assets/donghua/12.bmp"),
+];
 
 fn main() -> Result<()> {
     // 必须先调用，打补丁
@@ -32,9 +50,24 @@ fn main() -> Result<()> {
 
     primitives.fill_screen(WHITE)?;
 
-    println!("StatusBar已绘制完成！");
+    // println!("StatusBar已绘制完成！");
+
+    let bmps = DONGHUA_IMAGES
+        .iter()
+        .map(|&img| {
+            // 解析 BMP 文件
+            Bmp::<Rgb565>::from_slice(img).unwrap()
+        })
+        .collect::<Vec<_>>();
 
     loop {
-        FreeRtos::delay_ms(1000);
+        for bmp in &bmps {
+            primitives
+                .draw_image_at_grid(graphics::layout::GridPosition::MiddleCenter, bmp)
+                .unwrap();
+
+            // 添加延迟让动画更流畅，每帧间隔80ms
+            FreeRtos::delay_ms(80);
+        }
     }
 }
